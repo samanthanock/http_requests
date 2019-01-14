51fd92954340554e8cd75ea0604ceb8ca2d8175b
Samantha <sammarienock@gmail.com>
2019-01-14 21:55:03 +0000

    prints data to new string

---
+++ printing_w_buffered_strings.js
@@ -0,0 +1,22 @@
+var https = require('https');
+
+function getAndPrintHTML() {
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html',
+  };
+
+  https.get(requestOptions, function(response) {
+    response.setEncoding('utf8');
+    var dataString = '';
+
+    response.on('data', function(data) {
+      dataString = data.toString();
+      console.log(dataString);
+
+      response.on('end', function() {
+        console.log('Response stream complete. ');
+      });
+    });
+  });
+}
